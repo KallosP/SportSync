7cd28a53b814695b4be5a394ae37bec7fc274d5e
KallosP <pkallos19@gmail.com>
2025-07-11 16:09:25 -0700

    fix: add prod API URL

--- packages/project-frontend/src/constants.ts
+++ packages/project-frontend/src/constants.ts
@@ -1,4 +1,4 @@
-//const prodURL = ""
+const PROD_URL = "https://sportsync.azurewebsites.net"
 const LOCAL_URL = "http://localhost:3000";
-const BACKEND_URL = LOCAL_URL
+const BACKEND_URL = PROD_URL
 export default BACKEND_URL;
