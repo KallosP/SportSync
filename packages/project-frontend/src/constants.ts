bfdae930aa9474fdef2e407308836aec19a78b7b
KallosP <pkallos19@gmail.com>
2025-07-11 16:44:40 -0700

    fix: test

--- packages/project-frontend/src/constants.ts
+++ packages/project-frontend/src/constants.ts
@@ -1,4 +1,4 @@
 const PROD_URL = "https://sportsync.azurewebsites.net"
-const LOCAL_URL = "http://localhost:3000";
-const BACKEND_URL = LOCAL_URL
+//const LOCAL_URL = "http://localhost:3000";
+const BACKEND_URL = PROD_URL
 export default BACKEND_URL;
